71f3c4ca9a57697e9f047ba43cb5000b95498c51
Oleksandr Sheremet <oleksandr.sheremet@vitech.com.ua>
2017-12-25 19:06:51 +0200

    Add working functions signatures.

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,19 @@
+var winningPlaces = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    []
+]
+
+// state: string representing state of tic tac toe field (9 chars, "x", "o", " ")
 function isEndOfGame(state) {
+
+}
+
+// state: string representing state of tic tac toe field (9 chars, "x", "o", " ")
+// return: array of indexes of empty cells
+function possibleMoves (state) {
 
 }
