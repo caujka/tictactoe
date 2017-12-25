954bcff17cdd1ceb16ba294000a812d52f67ce30
Danny <liquidcore7@gmail.com>
2017-12-25 19:15:15 +0200

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,19 @@
 function isEndOfGame(state) {
-
+  function playerWon(state, player) {
+    var firstPos = state.find(player);
+    if (state[firstPos + 3] === player && state[firstPos + 6] === player)
+      return true;
+    if (state[firstPos + 1] === player && state[firstPos + 2] === player)
+      return true;
+    if (firstPos === 0 && state[4] === player && state[8] === player)
+      return true;
+    if (firstPos === 2 && state[4] === player && state[6] === player)
+      return true;
+    return false;
+  }
+  if (playerWon('x'))
+    return 'x'
+  if (playerWon('o'))
+    return 'o'
+  return '-'
 }
